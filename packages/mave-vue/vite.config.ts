7f1244883c3dc28eca61d8a9bfa806327a7dee90
David <davz0rs@gmail.com>
2022-05-11 15:18:55 +0200

    *bump*

--- packages/mave-vue/vite.config.ts
+++ packages/mave-vue/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
-      name: "mave-vue",
+      formats: ["es", "cjs"]
     },
     outDir: "dist",
     rollupOptions: {
